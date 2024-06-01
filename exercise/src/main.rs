39b51fe6963ffe7f40358d7418750f03b792eac4
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-06-01 14:31:04 -0400

    still leanring

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,3 +1,21 @@
+// fn main(){
+//     println!("Hello, world!");
+//     println!("I'm Rudy and I'm 21 and I love hip-hop.");
+// }
+
 fn main() {
-    println!("Hello, world! I am Rudy and I am learning Rust.");
+    let x: i32 = 21; // this in immutable; variables in rust are immutable by default
+    println!("my age is {} and via function is {}.", x, my_age_is());
+
+    // this would not work:
+    // x = 22 ( 😔 )
+}
+
+// example function, will learn more a bit later:
+// fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+//     return a * b + b * c + c * a;
+// }
+
+fn my_age_is() -> i32 {
+    21
 }
--- init.rs
+++ init.rs
@@ -20,5 +20,104 @@ optimized release build in target/release/.
 Add dependencies for your project by editing Cargo.toml. When you run cargo commands, it will automatically 
 download and compile missing dependencies for you.
 
+The types have widths as follows:
+    . iN, uN, and fN are N bits wide,
+    . isize and usize are the width of a pointer,
+    . char is 32 bits wide,
+    . bool is 8 bits wide.
 
+Rust has type inference - it determines type based on use and init value.
+When nothing constrains the type of an integer literal, Rust defaults to i32. 
+This sometimes appears as {integer} in error messages. Similarly, floating-point literals default to f64.
+
+if statements:
+you can use if as an expression. last expression of each block becomes the value of the if expression:
+let size = if x < 20 { "small" } else { "large" };
+
+for loop:
+can interate over ranges or elements.
+for x in 1..5 (1, 2, 3, 4) AND
+for elem in [1, 2, 3, 4, 5]
+
+loop:
+loop statements iterate until a break (akin to while True ig)
+let mut i = 0;
+    loop {
+        i += 1;
+        println!("{i}");
+        if i > 100 {
+            break;
+        }
+    }
+
+Both continue and break can optionally take a label argument which is used to break out of nested loops:
+    let s = [[5, 6, 7], [8, 9, 10], [21, 15, 32]];
+    let mut elements_searched = 0;
+    let target_value = 10;
+    'outer: for i in 0..=2 {        // note the 'outer label here, used by break to break out here
+        for j in 0..=2 {
+            elements_searched += 1;
+            if s[i][j] == target_value {
+                break 'outer;
+            }
+        }
+    }
+    print!("elements searched: {elements_searched}");
+
+Blocks:
+contains a sequence of expressions enclosed by braces {}.
+each block has its own value and type, which is that of the last expression in the block.
+    . hence, if the last expression ends with ;, then the resulting value and type is ().
+eg:
+    let z = 13;
+    let x = {
+        let y = 10;
+        println!("y: {y}");
+        z - y       // note the lack of ; in the last expression
+    };
+    println!("x: {x}");
+note that the scope of the block is limited to its enclosing block, so you can shadow variables from outside.
+
+Functions:
+fn gcd(a: u32, b: u32) -> u32 {
+    if b > 0 {
+        gcd(b, a % b)
+    } else {
+        a
+    }
+}
+
+fn main() {
+    println!("gcd: {}", gcd(143, 52));
+}
+Some functions have no return value, and return the ‘unit type’, (). The compiler will infer this if the -> () return type is omitted.
+
+Useful Macros:
+dbg!(...) -> logs the value of the expression and returns it
+
+Arrays:
+A value of the array type [T; N] holds N (a compile-time constant) elements of the same type T. 
+Note that the length of the array is part of its type, which means that [u8; 3] and [u8; 4] are 
+considered two different types.
+The println! macro asks for the debug implementation with the ? format parameter: {} gives the 
+default output, {:?} gives the debug output. Types such as integers and strings implement 
+the default output, but arrays only implement the debug output. This means that we must use 
+debug output here.
+Adding #, eg {a:#?}, invokes a “pretty printing” format, which can be easier to read.
+fn main() {
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a: {a:?}");
+}
+
+Tuples:
+Tuples group together values of different types into a compound type.
+Fields of a tuple can be accessed by the period and the index of the value, e.g. t.0, t.1.
+The empty tuple () is referred to as the “unit type” and signifies absence of a return value, 
+akin to void in other languages.
+fn main() {
+    let t: (i8, bool) = (7, true);
+    println!("t.0: {}", t.0);
+    println!("t.1: {}", t.1);
+}
 */
